28ad2801d86ce687a02784461369eb8be3327b2e
TianjiaSun <thomas.sun@outlook.com>
2015-03-19 23:08:48 -0700

    update edit page

--- app/js/controller.js
+++ app/js/controller.js
@@ -47,8 +47,6 @@ var customerApp = angular.module('customerApp',['ngRoute'])
       $("#editForm").hide();
       $("#informationForm").hide();
       $("#customerInformation").show();
-      $scope.expand_shipment_addr=false;
-      $scope.expand_mail_addr=false;
       $scope.selected = customer.id;
   }
   $scope.orderProp = 'customer_id';
@@ -69,30 +67,34 @@ var customerApp = angular.module('customerApp',['ngRoute'])
 
   $scope.clearInfo = function(customer) {
       $scope.customer=$scope.initial;
-      $scope.expand_shipment_addr=false;
-      $scope.expand_mail_addr=false;
   }
 
-
-  $scope.save = function(flag) {
-
-    console.log(flag);
-
-    if (flag === false){
+  $scope.newCustomer = function() {
       var req = {
         method: 'POST',
         url: 'http://asa.gausian.com',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        data: $.param({user_app_id:'app_id', service_app_name:'Customer',
-        request_string: 'update first='+$scope.customerUpdate.first+',last='+$scope.customerUpdate.last+
-        ',email='+$scope.customerUpdate.email+',company_name='+$scope.customerUpdate.company_name+
-        ',department_name='+$scope.customerUpdate.department_name+',work_phone='+$scope.customerUpdate.work_phone+
-        ',mobile_phone='+$scope.customerUpdate.mobile_phone+',shipment_address='+$scope.customerUpdate.shipment_address+
-        ',shipment_city='+$scope.customerUpdate.shipment_city+',shipment_state='+$scope.customerUpdate.shipment_state+
-        ',shipment_zip='+$scope.customerUpdate.shipment_zip+',shipment_country='+$scope.customerUpdate.shipment_country+';'})
+        data: $.param({
+          user_app_id:'app_id', 
+          service_app_name:'Customer',
+          request_string: 
+          'add first='+$scope.customerUpdate.first+
+          ' ,last='+$scope.customerUpdate.last+
+          ' ,email='+$scope.customerUpdate.email+
+          ' ,company_name='+$scope.customerUpdate.company_name+
+          ' ,department_name='+$scope.customerUpdate.department_name+
+          ' ,work_phone='+$scope.customerUpdate.work_phone+
+          ' ,mobile_phone='+$scope.customerUpdate.mobile_phone+
+          ' ,shipment_address='+$scope.customerUpdate.shipment_address+
+          ' ,shipment_city='+$scope.customerUpdate.shipment_city+
+          ' ,shipment_state='+$scope.customerUpdate.shipment_state+
+          ' ,shipment_zip='+$scope.customerUpdate.shipment_zip+
+          ' ,shipment_country='+$scope.customerUpdate.shipment_country
+        })
       }
+      console.log(req.data);
       $http(req).success(function(data) {
         //console.log(data.response);
         $scope.customers.push($scope.customerUpdate);
@@ -101,21 +103,32 @@ var customerApp = angular.module('customerApp',['ngRoute'])
         $("#informationForm").hide();
         $scope.selected = null;
       });
-    }
-    else{
+  };
+
+  $scope.updateCustomer = function() {
       var req = {
         method: 'POST',
         url: 'http://asa.gausian.com',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        data: $.param({user_app_id:'app_id', service_app_name:'Customer',
-        request_string: 'add first='+$scope.customerUpdate.first+',last='+$scope.customerUpdate.last+
-        ',email='+$scope.customerUpdate.email+',company_name='+$scope.customerUpdate.company_name+
-        ',department_name='+$scope.customerUpdate.department_name+',work_phone='+$scope.customerUpdate.work_phone+
-        ',mobile_phone='+$scope.customerUpdate.mobile_phone+',shipment_address='+$scope.customerUpdate.shipment_address+
-        ',shipment_city='+$scope.customerUpdate.shipment_city+',shipment_state='+$scope.customerUpdate.shipment_state+
-        ',shipment_zip='+$scope.customerUpdate.shipment_zip+',shipment_country='+$scope.customerUpdate.shipment_country+';ON:id==['+$scope.customerUpdate.id+'];'})
+        data: $.param({
+          user_app_id:'app_id', 
+          service_app_name:'Customer',
+          request_string: 
+          'update'+
+          ' email='+$scope.customerUpdate.email+
+          ' ,company_name='+$scope.customerUpdate.company_name+
+          ' ,department_name='+$scope.customerUpdate.department_name+
+          ' ,work_phone='+$scope.customerUpdate.work_phone+
+          ' ,mobile_phone='+$scope.customerUpdate.mobile_phone+
+          ' ,shipment_address='+$scope.customerUpdate.shipment_address+
+          ' ,shipment_city='+$scope.customerUpdate.shipment_city+
+          ' ,shipment_state='+$scope.customerUpdate.shipment_state+
+          ' ,shipment_zip='+$scope.customerUpdate.shipment_zip+
+          ' ,shipment_country='+$scope.customerUpdate.shipment_country+
+          ' on id='+$scope.customerUpdate.id
+        })
       }
       $http(req).success(function(data) {
         $scope.customer = angular.copy($scope.customerUpdate);
@@ -123,7 +136,6 @@ var customerApp = angular.module('customerApp',['ngRoute'])
         $("#informationForm").hide();
         $("#customerInformation").show();
       });
-    };
   };
 
   $scope.deleteCustomer = function() {
@@ -134,7 +146,7 @@ var customerApp = angular.module('customerApp',['ngRoute'])
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       data: $.param({user_app_id:'app_id', service_app_name:'Customer',
-      request_string: 'delete on id==['+$scope.customer.id+'];'})
+      request_string: 'delete id='+$scope.customer.id})
     }
     $http(req).success(function(data) {
       var index = $scope.customers.indexOf($scope.customer)
